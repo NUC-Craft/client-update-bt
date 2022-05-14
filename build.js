4897779308b2e46ff233a2ccc3f906a53dbb4116
Gaein nidb <Gaein_nidb_CN@outlook.com>
2022-05-14 15:04:56 +0800

    update build script

--- build.js
+++ build.js
@@ -7,15 +7,15 @@ import jsonfile from 'jsonfile';
 
 let version = '';
 
-spawn('cp', ['-r', TORRENTS_DIR, OUTPUT_DIR]);
 spawn('mkdir', ['-p', OUTPUT_DIR])
+spawn('cp', ['-r', TORRENTS_DIR, `${OUTPUT_DIR}/`]);
 
 jsonfile.readFile(META_FILE, (err, jsonData) => {
     if (err) throw err;
 
     version = jsonData['version'];
-    console.log(`version: ${version}`)
-    spawn('ln', ['-s', `client-${version}.torrent`, `${OUTPUT_DIR}/latest.torrent`]);
+    console.log(`version: $ { version }`)
+    spawn('ln', ['-s', `client-${version}.torrent`, `${OUTPUT_DIR}/${TORRENTS_DIR}/latest.torrent`]);
 })
 
 spawn('cp', [META_FILE, `${OUTPUT_DIR}/`])
